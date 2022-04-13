78ed723d6d4d61e9c415eeba780c6c3bbd07e6f8
Wilian <wmartzh@gmail.com>
2022-04-12 19:38:00 -0600

    Add new endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get('/hey',(req, res)=>{
+  res.send("Hey")
+})
+
 app.listen(3000, () => {
   console.log("Server running");
 });

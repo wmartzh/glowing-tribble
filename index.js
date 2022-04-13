15901d47421ff627d77876b31ebb9309c73329e5
wmartzh <wmartzh@gmai.com>
2022-04-12 19:40:44 -0600

    Add new message

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.get('/hey',(req, res)=>{
-  res.send("Hey")
-})
+app.get("/hey", (req, res) => {
+  res.send("Hey Pedro");
+});
 
 app.listen(3000, () => {
   console.log("Server running");
